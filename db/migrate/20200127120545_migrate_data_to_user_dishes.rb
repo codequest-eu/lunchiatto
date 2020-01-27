935e458e49202da0a0c3fd05846763dbebaaf520
hkopij <hkopijror@gmail.com>
2020-01-27 17:12:41 +0100

    fix: add ! to migration

--- db/migrate/20200127120545_migrate_data_to_user_dishes.rb
+++ db/migrate/20200127120545_migrate_data_to_user_dishes.rb
@@ -1,13 +1,13 @@
 class MigrateDataToUserDishes < ActiveRecord::Migration[5.0]
   def up  
     Dish.all.pluck(:user_id, :id).each do |user_id, id|
-      UserDish.create(user_id: user_id, dish_id: id)
+      UserDish.create!(user_id: user_id, dish_id: id)
     end
   end
   
   def down
     UserDish.all.pluck(:user_id, :dish_id).each do |user_id, dish_id|
-      Dish.find(dish_id).update(user_id: user_id)
+      Dish.find(dish_id).update!(user_id: user_id)
     end
     UserDish.delete_all
   end
