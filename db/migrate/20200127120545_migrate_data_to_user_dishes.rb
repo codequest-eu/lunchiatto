91e12234590dcaba71cdfb74a1876e491a798e56
hkopij <hkopijror@gmail.com>
2020-01-27 17:01:41 +0100

    fix: data migration

--- db/migrate/20200127120545_migrate_data_to_user_dishes.rb
+++ db/migrate/20200127120545_migrate_data_to_user_dishes.rb
@@ -1,7 +1,14 @@
 class MigrateDataToUserDishes < ActiveRecord::Migration[5.0]
-  def up
-    Dish.all.each do |dish|
-      UserDish.new(user_id: dish.user_id, dish_id: dish.id).save
+  def up  
+    Dish.all.pluck(:user_id, :id).each do |user_id, id|
+      UserDish.create(user_id: user_id, dish_id: id)
     end
+  end
+  
+  def down
+    UserDish.all.pluck(:user_id, :dish_id).each do |user_id, dish_id|
+      Dish.find(dish_id).update(user_id: user_id)
+    end
+    UserDish.delete_all
   end
 end
