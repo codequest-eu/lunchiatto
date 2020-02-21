d785673e725289363c97fb5b05d701b3cb9fc7a1
hkopij <hkopijror@gmail.com>
2020-02-21 11:18:36 +0100

    fix: change past user_dishes to always have dish_owner

--- db/migrate/20200129145837_add_dish_owner_to_user_dishes.rb
+++ db/migrate/20200129145837_add_dish_owner_to_user_dishes.rb
@@ -1,5 +1,5 @@
 class AddDishOwnerToUserDishes < ActiveRecord::Migration[5.0]
   def change
-    add_column :user_dishes, :dish_owner, :boolean, default: false
+    add_column :user_dishes, :dish_owner, :boolean, default: true
   end
 end
